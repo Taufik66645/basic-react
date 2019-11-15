863f31208adfc269b11c8962660f06d54152ab38
taufik66645 <taufik.66645@gmail.com>
2019-11-15 15:59:36 +0700

    Updates

---
+++ precache-manifest.cac3976059e22831ce10b6a1e5601c64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bf2d9f8d115908b9b8e368e078a915",
+    "url": "/basic-react/index.html"
+  },
+  {
+    "revision": "d0afc681eb7345eb8f27",
+    "url": "/basic-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b66c488e55b453ac32c5",
+    "url": "/basic-react/static/js/2.1e3d758a.chunk.js"
+  },
+  {
+    "revision": "d0afc681eb7345eb8f27",
+    "url": "/basic-react/static/js/main.375e1d3a.chunk.js"
+  },
+  {
+    "revision": "6b21fdc626792e74c49c",
+    "url": "/basic-react/static/js/runtime-main.e80c5eb4.js"
+  }
+]);
